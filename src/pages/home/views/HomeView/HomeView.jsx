f1f4b7ad60b3cd13987edf82db83e16ba2b85c48
Paula <paulacardone16@gmail.com>
2023-07-05 08:24:44 -0300

    visualización de las pelis en HomeView

--- src/pages/home/views/HomeView/HomeView.jsx
+++ src/pages/home/views/HomeView/HomeView.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { useAuth } from "../../../../hooks/useAuth";
 import { HomeSEO } from "./HomeSEO";
-import { Button, Container, Text } from "@nextui-org/react";
+import { Button, Container, Text, Card, Grid, Row } from "@nextui-org/react";
+import useSWR from "swr";
+import axios from "axios";
+
+const getMovies = async () => {
+  const movies = await axios("https://api.themoviedb.org/3/movie/popular?api_key=e44277a572d1f0c2b1e29b4f1ad5c401&language=es-ES&page=1");
+  return movies.data;
+};
 
 const HomeView = () => {
   const { logout, user } = useAuth();
@@ -9,6 +16,16 @@ const HomeView = () => {
   const handleLogout = () => {
     logout();
   };
+
+  const { data: movies, error, isLoading: isLoadingMovies } = useSWR("getMovies", getMovies); 
+  
+  if (error) {
+    return <div>Error al cargar las películas</div>;
+  }
+
+  if (isLoadingMovies){
+    return <div>Cargando...</div>
+  }
 
   return (
     <Container
@@ -19,23 +36,46 @@ const HomeView = () => {
       justifyContent: "center",
       height: "100vh",
     }}>
-    <div>
-      <h1><HomeSEO/></h1>
-      <Text
-        h4
-        color="secondary"
-      >
-        Bienvenido {user?.email}!
-      </Text>
-      <Button 
-        color="gradient" 
-        auto
-        onClick={handleLogout}
-      >
-        Cerrar Sesión
-      </Button>
+      <div>
+        <Card css={{ p: "$6", mw: "400px" }}>
+          <Card.Header>
+            <Grid.Container css={{ pl: "$6" }}>
+              <Grid xs={12}>
+                <Text h4 css={{ lineHeight: "$xs" }}>
+                  <HomeSEO/>
+                </Text>
+              </Grid>
+            </Grid.Container>
+          </Card.Header>
+          <Card.Body css={{ py: "$2" }}>
+            <Text
+              h4
+              color="secondary"
+            >
+              Bienvenido {user?.email}!
+            </Text>
+          </Card.Body>
+          <Card.Footer
+            css={{ 
+              display: "flex", 
+              justifyContent: "center" 
+            }}>
+            <Button 
+              color="gradient" 
+              auto
+              onClick={handleLogout}
+            >
+              Cerrar Sesión
+            </Button>
+          </Card.Footer>
+      </Card>
     </div>
-    </Container>
+    <div>
+      {movies.results.map((movie) => {
+        return <div key={movie.id}>{movie.title}</div>;
+      })}
+    </div>
+  </Container>
   );
 };
 
--- src/pages/series/views/SeriesView/SeriesView.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SeriesView = () => {
-  return (
-    <div>SeriesView</div>
-  )
-}
-
-export default SeriesView;
